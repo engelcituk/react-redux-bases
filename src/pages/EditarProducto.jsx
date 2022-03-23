a438999238b1d889144373a1614394905191eedf
cituk <ecituk@thefives.com.mx>
2022-03-22 23:36:39 -0500

    Creando la Edición de Productos y Listado

--- src/pages/EditarProducto.jsx
+++ src/pages/EditarProducto.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const EditarProducto = () => {
   return (
-    <div>EditarProducto</div>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">Editar producto</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre-producto">Nombre producto</label>
+                <input id="nombre-producto" type="text" className="form-control" placeholder="Nombre producto" name="nombre"/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="precio-producto">Precio del producto</label>
+                <input id="precio-producto" type="number" className="form-control" placeholder="Precio producto" name="precio"/>
+              </div>
+              <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+                Guardar cambios
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
