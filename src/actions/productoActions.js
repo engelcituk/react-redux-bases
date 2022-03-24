d0ad5bb848a8b6f987a2d7a7fb844de1e5690c41
cituk <ecituk@thefives.com.mx>
2022-03-23 22:39:55 -0500

    Definiendo el action y utilizando useDispatch

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,1 +1,8 @@
 import { AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_EXITO, AGREGAR_PRODUCTO_ERROR} from '../types'
+
+//crear nuevos productso
+export function crearNuevoProductoAction() {
+    return () => {
+        console.log('desde action')
+    }
+}
--- src/pages/NuevoProducto.jsx
+++ src/pages/NuevoProducto.jsx
@@ -1,12 +1,32 @@
+//Actions de redux
+import { crearNuevoProductoAction } from '../actions/productoActions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const NuevoProducto = () => {
+  //utilizar useDispatch y te crea una función
+  const dispatch = useDispatch()
+
+  //mandar llamar el action de productoActions
+  const agregarProduct = () => dispatch( crearNuevoProductoAction() )
+
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    //Validar formulario
+    //sino hay errores
+    //crear nuevo producto
+    agregarProduct()
+  }
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
         <div className="card">
           <div className="card-body">
             <h2 className="text-center mb-4 font-weight-bold">Agregar nuevo producto</h2>
-            <form>
+            <form
+              onSubmit={handleSubmit}
+            >
               <div className="form-group">
                 <label htmlFor="nombre-producto">Nombre producto</label>
                 <input id="nombre-producto" type="text" className="form-control" placeholder="Nombre producto" name="nombre"/>
