ae4b9e1398d7c53eab5dbc28f6ea07e86f958b62
cituk <ecituk@thefives.com.mx>
2022-03-25 00:54:04 -0500

    Creando un segundo reducer

---
+++ src/reducers/alertaReducer.js
@@ -0,0 +1,23 @@
+import { MONSTRAR_ALERTA, OCULTAR_ALERTA } from '../types'
+
+//cada reducer tiene su propio state
+const initialState = {
+    alerta: null,
+}
+
+export default function ( state = initialState, action) {
+    switch ( action.type ) {
+        case MONSTRAR_ALERTA:
+            return {
+                ...state,
+                alerta: action.payload,
+            }
+        case OCULTAR_ALERTA:
+            return {
+                ...state,
+                alerta: null,
+            }
+        default:
+            return state;
+    }
+}
