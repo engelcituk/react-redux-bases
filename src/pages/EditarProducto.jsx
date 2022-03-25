873d3cd482b1dfe693583e2126f963f59971c7da
cituk <ecituk@thefives.com.mx>
2022-03-25 00:01:10 -0500

    Llenando el Formulario

--- src/pages/EditarProducto.jsx
+++ src/pages/EditarProducto.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const EditarProducto = () => {
+
+  const producto = useSelector( state => state.productos.productoEditar )
+  if(!producto) return null
+  const { nombre, precio, id } = producto
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -10,11 +15,25 @@ const EditarProducto = () => {
             <form>
               <div className="form-group">
                 <label htmlFor="nombre-producto">Nombre producto</label>
-                <input id="nombre-producto" type="text" className="form-control" placeholder="Nombre producto" name="nombre"/>
+                <input
+                  id="nombre-producto"
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre producto"
+                  name="nombre"
+                  value={nombre}
+                />
               </div>
               <div className="form-group">
                 <label htmlFor="precio-producto">Precio del producto</label>
-                <input id="precio-producto" type="number" className="form-control" placeholder="Precio producto" name="precio"/>
+                <input
+                  id="precio-producto"
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio producto"
+                  name="precio"
+                  value={precio}
+                />
               </div>
               <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
                 Guardar cambios
